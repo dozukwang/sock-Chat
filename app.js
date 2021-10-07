4bfe50a593cb46817f886c748d50ded18dc81813
dozukwang <dozukwang@gmail.com>
2021-10-07 18:19:36 +0900

    feat: Route & Render for /chat
    chat을 위한 공간 형성, 기능은 아직 X

--- app.js
+++ app.js
@@ -11,12 +11,15 @@ app.set("views", path.join(__dirname, "views"));
 app.use("/public", express.static(__dirname + "/public"))
 
 //router 묶기
-const indexRouter = require('./routes/index') //확장자 없이 작성
-const clubsRouter = require('./routes/clubs')
+const indexRouter = require('./routes/index'); //확장자 없이 작성
+const clubsRouter = require('./routes/clubs');
+const chatRouter = require('./routes/chat');
+
+
 
 app.use('/', indexRouter); //mian에 들어올려 할 때 저거 실행
 app.use('/clubs', clubsRouter); //clubs라우터에 들어오려 할 때 저거실행
-
+app.use('/chat', chatRouter);
 
 // IndexRouter이라는 걸 만들어서 메인으로 들어오려고 하면 indexRouter을 확인하도록
 
---
+++ routes/chat.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const express = require('express');
+const router = express.Router();
+
+//router의 어느 경로('/')로 들어왔을 때 get 처리를 하겠다
+router.route('/').get((req, res, next) => {
+  //get에서 어떤 일을 할 것인가
+  res.render("chatroom");
+})
+
+
+module.exports = router;

029e497f5fc7e8e9418a69380d800fb732b86d2b
dozukwang <dozukwang@gmail.com>
2021-10-07 17:48:17 +0900

    feats: Render ejs with indexRouter
    requirement: ejs
    ejs는 npm install --save ejs로 진행

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const path = require('path');
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 
 //router 묶기
 const indexRouter = require('./routes/index') //확장자 없이 작성
 const clubsRouter = require('./routes/clubs')
-
-
 
 app.use('/', indexRouter); //mian에 들어올려 할 때 저거 실행
 app.use('/clubs', clubsRouter); //clubs라우터에 들어오려 할 때 저거실행
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 //router의 어느 경로('/')로 들어왔을 때 get 처리를 하겠다
 router.route('/').get((req, res, next) => {
   //get에서 어떤 일을 할 것인가
-  return res.send("indexRouter works!");
+  res.render("index");
 })
 
 

a69236c002239873a40b141289384af14ead8ed2
dozukwang <dozukwang@gmail.com>
2021-10-07 18:59:08 +0900

    feat: Broadcasting message ( socketio done )
    웹화면에 메세지가 전달되어 표시되도록 설정
    이후 히로쿠 등을 연결하면 타인이 입장 가능한 서버 형성 가능...
    나중에 strapi 등에 대해서도 공부하기

--- socket.js
+++ socket.js
@@ -3,9 +3,10 @@ module.exports = (io) => {
     console.log('A user is now connected')
 
     sockek.on('chat message', (msg) => {
-      console.log("message:", msg);
+      io.emit('chat message', msg); // 서버에 연결된 모든 사용자(클라)에게 메세지 전달하는 것(일방향 통신)
+      console.log("message:", msg); // 콘솔창에 입력한거 뜨게(자신의)
     });
-    
+
     sockek.on('disconnect', () => {
       console.log('disconnected')
     });

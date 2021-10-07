9cc01fc33b156f468f9faf287c0ef0e794667c4e
dozukwang <dozukwang@gmail.com>
2021-10-07 16:37:54 +0900

    feat: Add indexRouter
    splited app.js and index.js router

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
-app.get('/', (req, res) => { //메인페이지에 접근하려고 할 때 이런 것을 하겠다
-  res.send('Express work!');
-  
-});
+const indexRouter = require('./routes/index') //확장자 없이 작성
+
+app.use('/', indexRouter);
+
+// IndexRouter이라는 걸 만들어서 메인으로 들어오려고 하면 indexRouter을 확인하도록
 
 server.listen(3000, () => {
   console.log('Express Server is now listening on local host:3000..')
